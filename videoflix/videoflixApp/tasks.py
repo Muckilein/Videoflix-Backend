d4da2a39cc2580fe12581d2ae84d680fda1a6627
Muckilein <JuliaWessolleck@t-online.de>
2024-08-20 19:31:08 +0000

    dataserver

--- videoflix/videoflixApp/tasks.py
+++ videoflix/videoflixApp/tasks.py
@@ -4,17 +4,23 @@ def convert_480p(source):
     new_file_name = source+"_480.mp4" 
     print(source)
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)  # {} wird durch dass was bei source bzw. target angegeben ist ersetzt  
-    run = subprocess.run(cmd, capture_output=True)
+    run = subprocess.run(cmd,shell=True, text=True, capture_output=True)
+#    print("Return code:", run.returncode)
+ #   if run.stdout:
+  #      print("Standard output:", run.stdout)
+   # if run.stderr:
+    #    print("Standard error:", run.stderr)
     
     
 def convert_720p(source):    
     new_file_name = source+"_720.mp4" 
     print(source)
     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
-    run = subprocess.run(cmd, capture_output=True)
-    
+#    run = subprocess.run(cmd, capture_output=True)
+    run = subprocess.run(cmd,shell=True, text=True, capture_output=True)    
 def convert_1080p(source):    
     new_file_name = source+"_1080.mp4" 
     print(source)
     cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
-    run = subprocess.run(cmd, capture_output=True)
+    #run = subprocess.run(cmd, capture_output=True)
+    run = subprocess.run(cmd,shell=True, text=True, capture_output=True)
